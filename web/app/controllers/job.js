7f55b0258fe4d0aa69d27700113a130cf7a40548
seba <seba@goempirical.com>
2017-09-11 23:30:23 -0700

    adding a simple handler to start a process from a web POST

--- web/app/controllers/job.js
+++ web/app/controllers/job.js
@@ -1,5 +1,7 @@
 const service = require('../services/job')
 
+
+
 exports.list = function(req, res){
   const response = service.list();
   res.json(response);
@@ -9,5 +11,6 @@ exports.create = function(req, res) {
   console.log("jobService::create()");
   const job = req.body;
   const response = service.create(job);
+  console.log("This is the response: " + response)
   res.json(response);
 }
--- web/app/services/job.js
+++ web/app/services/job.js
@@ -1,14 +1,29 @@
-
-
-
+const execAsync = require('async-child-process').execAsync;
+const spawn = require('child_process').spawn;
 
 exports.list = function (){
   return "This is the response from this method Job::List()";
 }
 
 exports.create = function(job){
-  console.log("Starting a screencast-producer");
-  console.log("Starting a queue");
-  console.log("starting a screencast-consumer")
+  console.log("Starting a job");
+  try {
+    //response = spawn("node ../engine/index.js 'https://www.youtube.com/watch?v=5-prFsuWdqs' 0 awesomeness");
+    const ops = getOpts(job);
+    screencaster = spawn('node', ops, { stdio: [ 'pipe', 'pipe', 2 ], cwd: "../engine"} );
+  }catch(err){
+    console.log("Failed badly to start the process " + err);
+    return err;
+  }
   return `We have scheduled a job to screencast this url: ${job.url}`;
+}
+
+function getOpts(job){
+  const options = [
+    '../engine/index.js',
+    job.url, // url to screencast
+    '0', // audio offset
+    job.outputName // output name
+  ];
+  return options;
 }
