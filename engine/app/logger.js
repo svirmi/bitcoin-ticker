d953e67809601eaa1e6c03b4220566ba480bd9aa
Sebastian Pereyro <seba@goempirical.com>
2017-10-17 22:57:31 -0700

    adding winston

--- engine/app/logger.js
+++ engine/app/logger.js
@@ -1,3 +1,17 @@
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
 exports.log = function (message){
-  console.log( "[Bullman] " + message );
+  //console.log( "[Bullman] " + message );
+  //
+  // Logging
+  //
+  logger.log({
+    level: 'info',
+    message: "[Bullman] " + message
+  });
 }
