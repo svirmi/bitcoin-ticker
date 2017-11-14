48f0fe2ef0601eb311abd03517dc0e4420b821b6
Sebastian Pereyro <seba@goempirical.com>
2017-11-13 22:32:21 -0800

    making wait

--- engine/app/screencaster.js
+++ engine/app/screencaster.js
@@ -42,7 +42,26 @@ exports.start = async function(q) {
   // Wait for window.onload before start streaming.
   await Page.loadEventFired(async () => {
     logger.log("Page.loadEventFired onload fired");
-    await executeAfterPageLoaded(chrome, sinkId);
+
+    // Waiting so we are loading the sound
+    // pushing this further, vimeo take s alot of time to load the audio
+    await execAsync('sleep 17');
+
+    // GetInputId
+    const inputIdList = await pulseaudio.getInputId(chrome.pid);
+
+    for (i = 0; i < inputIdList.length; i++) {
+      var inputId = inputIdList[i];
+      // move input to its corresponding sink
+      await pulseaudio.moveInput(inputId, sinkId);
+    }
+    
+    //Start capturing frames
+    await startCapturingFrames();
+
+    var params = ffmpegProcessParams(stats.getStats.currentFPS, args.getAudioOffset(), args.getOutputName(), args.getRtmpUrl(), null)
+    ffmpeg = ffmpegLauncher.start(params);
+
   });
 
 }
@@ -56,29 +75,6 @@ async function initPulseAudio(){
 
   // Create a new audio sink for this stream
   return await pulseaudio.createSink(args.getOutputName());
-}
-
-async function executeAfterPageLoaded(chrome, sinkId){
-
-  // Waiting so we are loading the sound
-  // pushing this further, vimeo take s alot of time to load the audio
-  await execAsync('sleep 17');
-
-  // GetInputId
-  const inputIdList = await pulseaudio.getInputId(chrome.pid);
-
-  for (i = 0; i < inputIdList.length; i++) {
-    var inputId = inputIdList[i];
-    // move input to its corresponding sink
-    await pulseaudio.moveInput(inputId, sinkId);
-  }
-
-  //Start capturing frames
-  await startCapturingFrames();
-
-  var params = ffmpegProcessParams(stats.getStats.currentFPS, args.getAudioOffset(), args.getOutputName(), args.getRtmpUrl(), null)
-  ffmpeg = ffmpegLauncher.start(params);
-
 }
 
 function initRemoteInterface(chrome){
